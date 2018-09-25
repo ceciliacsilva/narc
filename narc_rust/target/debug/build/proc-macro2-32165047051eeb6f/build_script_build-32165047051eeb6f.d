35aaad0e366ef4b5baab42fc50a67d2226121f8d
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2018-09-25 13:54:57 -0300

    Cargo.toml, Readme: fixing some things

--- narc_rust/target/debug/build/proc-macro2-32165047051eeb6f/build_script_build-32165047051eeb6f.d
+++
@@ -1,5 +0,0 @@
-/home/ceciliacarneiro/ufu/mestrado/pesquisa/rust_embedded_after_paper/moi/narc/quickstart/narc_rust/target/debug/build/proc-macro2-32165047051eeb6f/build_script_build-32165047051eeb6f: /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.19/build.rs
-
-/home/ceciliacarneiro/ufu/mestrado/pesquisa/rust_embedded_after_paper/moi/narc/quickstart/narc_rust/target/debug/build/proc-macro2-32165047051eeb6f/build_script_build-32165047051eeb6f.d: /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.19/build.rs
-
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.19/build.rs:
--- narc_rust/target/debug/deps/rand_core-2a0ccc22d7cd8fa6.d
+++
@@ -1,9 +0,0 @@
-/home/ceciliacarneiro/ufu/mestrado/pesquisa/rust_embedded_after_paper/moi/narc/quickstart/narc_rust/target/debug/deps/librand_core-2a0ccc22d7cd8fa6.rlib: /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/lib.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/error.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/block.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/impls.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/le.rs
-
-/home/ceciliacarneiro/ufu/mestrado/pesquisa/rust_embedded_after_paper/moi/narc/quickstart/narc_rust/target/debug/deps/rand_core-2a0ccc22d7cd8fa6.d: /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/lib.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/error.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/block.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/impls.rs /home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/le.rs
-
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/lib.rs:
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/error.rs:
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/block.rs:
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/impls.rs:
-/home/ceciliacarneiro/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.1/src/le.rs:

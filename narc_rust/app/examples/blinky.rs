89a74131203d26dd73d87f07d183e59dfa2f690b
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2018-10-22 13:37:49 -0300

    examples - blink pwm, led io

--- narc_rust/app/examples/blinky.rs
+++ narc_rust/app/examples/blinky.rs
@@ -41,29 +41,33 @@ fn blinky() {
     let mut gpioa = hw.GPIOA.split(&mut rcc.iop);
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // let led = gpioa.pa5.into_alternate(&mut gpioa.moder).af5(&mut gpioa.afrl);
+    let led = gpioa.pa5.into_alternate(&mut gpioa.moder).af5(&mut gpioa.afrl);
 
-    // let mut pwm = hw.TIM2
-    //                 .pwm(
-    //                     led,
-    //                     1.hz(),
-    //                     clocks,
-    //                     &mut rcc.apb1,
-    //                 );
+    let mut pwm = hw.TIM2
+                    .pwm(
+                        led,
+                        1.hz(),
+                        clocks,
+                        &mut rcc.apb1,
+                    );
 
-    // let max = pwm.get_max_duty();
-    // pwm.enable();
-    // pwm.set_duty(max / 2);
+    let max = pwm.get_max_duty();
+    pwm.enable();
+    pwm.set_duty(max / 2);
 
-    let mut led = gpioa.pa5.into_output(&mut gpioa.moder).push_pull(&mut gpioa.otyper);
-    let bot = gpioa.pa4.into_input(&mut gpioa.moder).pull_up(&mut gpioa.pupdr);
+    // let mut led = gpioa.pa5.into_output(&mut gpioa.moder).push_pull(&mut gpioa.otyper);
+    // let bot = gpioa.pa4.into_input(&mut gpioa.moder).pull_up(&mut gpioa.pupdr);
+
+    // loop{
+    //     if bot.is_high(){
+    //         led.set_high();
+    //     } else {
+    //         led.set_low();
+    //     }
+    // }
 
     loop{
-        if bot.is_high(){
-            led.set_high();
-        } else {
-            led.set_low();
-        }
+
     }
 }
 
